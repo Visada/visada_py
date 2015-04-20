dea84cef85dd63ed2946eccf5e6c745cf144ca48
Issac Trotts <issac.trotts@gmail.com>
2015-04-19 22:21:16 -0700

    Add installation of requests and visada

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Visada',
+      version='1.0',
+      description='Visada API',
+      author='Praveen Srinivasan',
+      author_email='',
+      url='https://github.com/visada/visada_py',
+      packages=['visada'],
+      requires=['requests']
+      )
+
